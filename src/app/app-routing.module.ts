f60e9a2687e8aa616f4e71085437e6ef11afe7b8
Rafael Reis <rafa.araujoreis@gmail.com>
2022-06-30 18:34:56 -0300

    add routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FaqComponent } from './pages/faq/faq.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'faq',
+    component: FaqComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
