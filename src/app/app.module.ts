63dee327102996b335484ed189bfddebe0ff9abf
Rafael Reis <rafa.araujoreis@gmail.com>
2022-06-30 18:35:31 -0300

    imports pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccordionComponent } from './components/accordion/accordion.component';
-
+import { PagesModule } from './pages/pages.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MaterialModule } from './shared/material.module';
 @NgModule({
   declarations: [
     AppComponent,
-    AccordionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  exports: [
+    MaterialModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
